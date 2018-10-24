89df74d7e9078739cd34119b9431b1948769c69b
Alexey Kolpakov <al.kolpak@gmail.com>
2018-10-24 20:38:29 +0300

    thelocals indev

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const pSettle = require('p-settle');
 const cian = require('./lib/provider/cian');
 const yandex = require('./lib/provider/yandex');
 const avito = require('./lib/provider/avito');
+// const thelocals = require('./lib/provider/thelocals');
 
 run();
 
@@ -12,6 +13,7 @@ async function run() {
         Promise.all([Promise.resolve('cian'), cian()]),
         Promise.all([Promise.resolve('yandex'), yandex()]),
         Promise.all([Promise.resolve('avito'), avito()]),
+        // Promise.all([Promise.resolve('thelocals'), thelocals()]),
     ]).then((result) => {
         console.info(new Date().toString());
         result.forEach((x) => {
